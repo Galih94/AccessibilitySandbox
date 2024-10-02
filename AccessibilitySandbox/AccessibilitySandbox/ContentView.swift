1e5a477c07479b6a81e59853be0d920ab5667b8f
galih <samudragalih94@gmail.com>
2024-10-02 10:05:49 +0700

    feat(VoiceIver): Handling voice input using `accessibilityInputLabels`

--- AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello world")
+        Button("John Fitzgerald Kennedy") {
+            print("button tapped")
+        }
+        .accessibilityInputLabels(["John Fitzgerald Kennedy", "Kennedy", "JFK"])
     }
 }
 
